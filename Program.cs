bda162253104fa53829c1dc5a4bb2a7654658714
Elliott Pergande <epergande@wctc.msft>
2025-02-10 12:49:44 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,157 @@
+﻿// ask for input 
+
+Console.WriteLine("Enter 1 to create data file."); 
+
+Console.WriteLine("Enter 2 to parse data."); 
+
+Console.WriteLine("Enter anything else to quit."); 
+
+// input response 
+
+string? resp = Console.ReadLine(); 
+
+ 
+
+if (resp == "1") 
+
+{ 
+
+    // create data file 
+
+ 
+
+    // ask a question 
+
+    Console.WriteLine("How many weeks of data is needed?"); 
+
+    // input the response (convert to int) 
+
+    int weeks = Convert.ToInt32(Console.ReadLine()); 
+
+    // determine start and end date 
+
+    DateTime today = DateTime.Now; 
+
+    // we want full weeks sunday - saturday 
+
+    DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek); 
+
+    // subtract # of weeks from endDate to get startDate 
+
+    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7)); 
+
+    // random number generator 
+
+    Random rnd = new(); 
+
+    // create file 
+
+    StreamWriter sw = new("data.txt"); 
+
+ 
+
+    // loop for the desired # of weeks 
+
+    while (dataDate < dataEndDate) 
+
+    { 
+
+        // 7 days in a week 
+
+        int[] hours = new int[7]; 
+
+        for (int i = 0; i < hours.Length; i++) 
+
+        { 
+
+            // generate random number of hours slept between 4-12 (inclusive) 
+
+            hours[i] = rnd.Next(4, 13); 
+
+        } 
+
+        // M/d/yyyy,#|#|#|#|#|#|# 
+
+        Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}"); 
+
+        // Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}"); 
+
+        sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}"); 
+
+        // add 1 week to date 
+
+        dataDate = dataDate.AddDays(7); 
+
+    } 
+
+    sw.Close(); 
+
+} 
+
+else if (resp == "2") 
+
+{ 
+
+    /*        if (File.Exists(file)) 
+
+        { 
+
+            StreamReader sr = new(file); 
+
+            while (!sr.EndOfStream) 
+
+            { 
+
+                string? line = sr.ReadLine(); 
+
+                string[] arr = String.IsNullOrEmpty(line) ? [] : line.Split(','); 
+
+                Console.WriteLine("ID: {0}", arr[0],arr[1],arr[2]); 
+
+                Console.WriteLine("Name: {1}",arr[0],arr[1],arr[2]); 
+
+                Console.WriteLine("Relation to mario: {2}",arr[0],arr[1],arr[2]); 
+
+            } 
+
+            sr.Close(); 
+
+ 
+
+        } 
+
+        else 
+
+        { 
+
+            Console.WriteLine("File does not exist"); 
+
+        }*/ 
+
+       if (File.Exists(sw)) 
+
+        { 
+
+            StreamReader sr = new(sw); 
+
+            while (!sr.EndOfStream) 
+
+            { 
+
+                string? line = sr.ReadLine(); 
+
+                string[] arr = String.IsNullOrEmpty(line) ? [] : line.Split(','); 
+
+                Console.WriteLine($"Week of {today:MMM},{today:dd},{today:yyyy}"); 
+
+            } 
+
+            sr.Close(); 
+
+ 
+
+        } 
+
+} 
+
+ 
