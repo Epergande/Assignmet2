cb3ce32b849ed7d65210780bafaa6dec3705d84f
Epergande <elliott.pergande1@gmail.com>
2025-02-13 11:18:35 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,188 +1,81 @@
-﻿// ask for input 
-
-Console.WriteLine("Enter 1 to create data file."); 
-
-Console.WriteLine("Enter 2 to parse data."); 
-
-Console.WriteLine("Enter anything else to quit."); 
-
-// input response 
-
-string? resp = Console.ReadLine(); 
-
- 
-
-if (resp == "1") 
-
-{ 
-
-    // create data file 
-
- 
-
-    // ask a question 
-
-    Console.WriteLine("How many weeks of data is needed?"); 
-
-    // input the response (convert to int) 
-
-    int weeks = Convert.ToInt32(Console.ReadLine()); 
-
-    // determine start and end date 
-
-    DateTime today = DateTime.Now; 
-
-    // we want full weeks sunday - saturday 
-
-    DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek); 
-
-    // subtract # of weeks from endDate to get startDate 
-
-    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7)); 
-
-    // random number generator 
-
-    Random rnd = new(); 
-
-    // create file 
-
-    StreamWriter sw = new("data.txt"); 
-
- 
-
-    // loop for the desired # of weeks 
-
-    while (dataDate < dataEndDate) 
-
-    { 
-
-        // 7 days in a week 
-
-        int[] hours = new int[7]; 
-
-        for (int i = 0; i < hours.Length; i++) 
-
-        { 
-
-            // generate random number of hours slept between 4-12 (inclusive) 
-
-            hours[i] = rnd.Next(4, 13); 
-
-        } 
-
-        // M/d/yyyy,#|#|#|#|#|#|# 
-
-        Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}"); 
-
-        // Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}"); 
-
-        sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}"); 
-
-        // add 1 week to date 
-
-        dataDate = dataDate.AddDays(7); 
-
-    } 
-
-    sw.Close(); 
-
-} 
-
-else if (resp == "2") 
-
-{ 
-
-    /*        if (File.Exists(file)) 
-
-        { 
-
-            StreamReader sr = new(file); 
-
-            while (!sr.EndOfStream) 
-
-            { 
-
-                string? line = sr.ReadLine(); 
-
-                string[] arr = String.IsNullOrEmpty(line) ? [] : line.Split(','); 
-
-                Console.WriteLine("ID: {0}", arr[0],arr[1],arr[2]); 
-
-                Console.WriteLine("Name: {1}",arr[0],arr[1],arr[2]); 
-
-                Console.WriteLine("Relation to mario: {2}",arr[0],arr[1],arr[2]); 
-
-            } 
-
-            sr.Close(); 
-
- 
-
-        } 
-
-        else 
-
-        { 
-
-            Console.WriteLine("File does not exist"); 
-
-        }*/ 
-
-       if (File.Exists(sw)) 
-
-        { 
-
-            StreamReader sr = new(sw); 
-
-            while (!sr.EndOfStream) 
-
-            { 
-
-                string? line = sr.ReadLine(); 
-
-                string[] arr = String.IsNullOrEmpty(line) ? [] : line.Split(','); 
-
-                Console.WriteLine($"Week of {today:MMM},{today:dd},{today:yyyy}"); 
-
-            } 
-
-            sr.Close(); 
-
- /*else if (resp == "2") 
-
-{ 
- StreamReader sr = new("data.txt");
-
-            if (File.Exists(sr)) 
-
-        { 
-
-        
-            StreamReader sr = new(sr);
-            while (!sr.EndOfStream)
-            {
-                string? line = sr.ReadLine();
-                string[] arr = String.IsNullOrEmpty(line) ? [] : line.Split('|');
-
-                 Console.WriteLine($"Week of {today:MMMM,dd,yyyy}");
-                 Console.WriteLine("Su  Mo  Tu  We  Th  Fr  Sa");
-                 Console.WriteLine("--  --  --  --  --  --  --");
-                
-
-            }
-            sr.Close();
-
-        }
-        else
+‎Program.cs
++4
+-1
+Original file line number	Diff line number	Diff line change
+@@ -1,68 +1,71 @@
+﻿// ask for input
+Console.WriteLine("Enter 1 to create data file.");
+Console.WriteLine("Enter 2 to parse data.");
+Console.WriteLine("Enter anything else to quit.");
+// input response
+string? resp = Console.ReadLine();
+
+if (resp == "1")
+{
+    // create data file
+
+    // ask a question
+    Console.WriteLine("How many weeks of data is needed?");
+    // input the response (convert to int)
+    int weeks = Convert.ToInt32(Console.ReadLine());
+    // determine start and end date
+    DateTime today = DateTime.Now;
+    // we want full weeks sunday - saturday
+    DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+    // subtract # of weeks from endDate to get startDate
+    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+    // random number generator
+    Random rnd = new();
+    // create file
+    StreamWriter sw = new("data.txt");
+
+    // loop for the desired # of weeks
+    while (dataDate < dataEndDate)
+    {
+        // 7 days in a week
+        int[] hours = new int[7];
+        for (int i = 0; i < hours.Length; i++)
         {
-            Console.WriteLine("File does not exist");
+            // generate random number of hours slept between 4-12 (inclusive)
+            hours[i] = rnd.Next(4, 13);
         }
-                
+        // M/d/yyyy,#|#|#|#|#|#|#
+        Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+        // Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+        sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
+        // add 1 week to date
+        dataDate = dataDate.AddDays(7);
+    }
+    sw.Close();
+}
 
 
-} 
 
- */
+
+else if (resp == "2")
+{
+    List<string> dates = new List<string>();
+    List<string> hours = new List<string>();
+    string file = "data.txt";
+
+    StreamReader reader = new StreamReader(file);
+    string? line = reader.ReadLine();
+
+while(line != null)
+{
+    string[] splitdate = line.Split(',');
+    string[] date = line.Split('/');
+    string[] splithour = line.Split('|');
+    Console.WriteLine("Week of {0:MMM},{1:dd},{2:yyyy}",date[0],date[1],date[2]);
+    Console.WriteLine("Week of {0:MMM} {1:dd} {2:yyyy}",date[0],date[1],date[2]);
+    Console.WriteLine("Su  Mo  Tu  We  Th  Fr  Sa");
+    Console.WriteLine("--  --  --  --  --  --  --");
+    Console.WriteLine("{0}  {1}  {2}  {3}  {4}  {5}  {6}\n",splithour[0],splithour[1],splithour[2],splithour[3],splithour[4],splithour[5],splithour[6]);
+    line = reader.ReadLine();
+
+}
+
+‎data.txt
 
         } 
 
